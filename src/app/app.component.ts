145b16e3f7ec1d2387265caec921a2d2072ab51b
Muhannad Fathi <muhannad.m.fathi@gmail.com>
2024-12-11 22:53:42 +0200

    Configurable Component Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   usersArray = DUMMY_USERS;
+  selectedUserID = 'u1';
 
   onSelectedUser(id: string){
-    console.log(`Selected User ID : ${id}` );
+    this.selectedUserID = id ;
   }
+
+  get selectedUser(){
+    return this.usersArray.find(( user => user.id === this.selectedUserID))!;
+  }
+
 }
